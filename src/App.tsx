c1047bdb0f1648be5403ddd724db7ef2f234f4e8
Daeuni <ekdmsjj@gmail.com>
2024-12-07 00:37:32 +0900

    setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const App: React.FC = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log('Effect 실행: count =', count);
+
+    // 클린업 함수 정의
+    return () => {
+      console.log('클린업 실행: count =', count);
+    };
+  }, [count]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>증가</button>
     </div>
   );
-}
+};
 
 export default App;
